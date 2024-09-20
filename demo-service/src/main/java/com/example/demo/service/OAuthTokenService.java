9c9ea8364f9b16cec626efd867d248578bbd3a07
Kishore Shiraguppi <kishor4u@gmail.com>
2024-09-19 23:20:50 -0400

    Auto commit

--- demo-service/src/main/java/com/example/demo/service/OAuthTokenService.java
+++ demo-service/src/main/java/com/example/demo/service/OAuthTokenService.java
@@ -1,8 +1,20 @@
 package com.example.demo.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OAuthTokenService {
-    // ... service code ...
+
+    private static final Logger logger = LoggerFactory.getLogger(OAuthTokenService.class);
+
+    @Value("${github.personal-access-token}")
+    private String personalAccessToken;
+
+    public String getAccessToken() {
+        logger.info("Returning GitHub personal access token");
+        return personalAccessToken;
+    }
 }
